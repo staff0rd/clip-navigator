d6089cedcf96c2cf1e57db6a8e02c4f6bf38d93c
Stafford Williams <stafford.williams@gmail.com>
2021-01-27 22:11:48 +1100

    style

--- src/App.js
+++ src/App.js
@@ -4,9 +4,20 @@ import FormControl from "@material-ui/core/FormControl";
 import Select from "@material-ui/core/Select";
 import InputLabel from "@material-ui/core/InputLabel";
 import MenuItem from "@material-ui/core/MenuItem";
+import { makeStyles } from "@material-ui/core/styles";
 import data from "./data";
 
+const useStyles = makeStyles((theme) => ({
+  root: {
+    marginTop: theme.spacing(1),
+  },
+  selectors: {
+    marginBottom: theme.spacing(1),
+  },
+}));
+
 function App() {
+  const classes = useStyles();
   const [currentVideoId, setCurrentVideoId] = useState(data[0].videoId);
   const [currentVideo, setCurrentVideo] = useState(
     data.find((v) => v.videoId === currentVideoId)
@@ -37,25 +48,27 @@ function App() {
   };
 
   return (
-    <div>
-      <FormControl variant="outlined">
-        <InputLabel id="video-label">Video</InputLabel>
-        <Select
-          labelId="video-label"
-          id="video-select"
-          value={currentVideoId}
-          onChange={(e) => {
-            setCurrentVideoId(e.target.value);
-          }}
-          label="Video"
-        >
-          {data.map((v, ix) => (
-            <MenuItem key={`video-${ix}`} value={v.videoId}>
-              {v.name}
-            </MenuItem>
-          ))}
-        </Select>
-      </FormControl>
+    <div className={classes.root}>
+      <div className={classes.selectors}>
+        <FormControl variant="outlined">
+          <InputLabel id="video-label">Video</InputLabel>
+          <Select
+            labelId="video-label"
+            id="video-select"
+            value={currentVideoId}
+            onChange={(e) => {
+              setCurrentVideoId(e.target.value);
+            }}
+            label="Video"
+          >
+            {data.map((v, ix) => (
+              <MenuItem key={`video-${ix}`} value={v.videoId}>
+                {v.name}
+              </MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
       <YoutubePlayer
         video={currentVideo}
         newClip={newClipCallback}
